fe2539d0ced1207aa287d860deeac9e24302137e
Breno <breno.cpedroso@gmail.com>
2025-03-15 20:00:38 -0300

    Add relationships and visit_count to Hotwheels model

--- src/hotwheels/models.py
+++ src/hotwheels/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, UUID, TIMESTAMP, Index
 from sqlalchemy.sql import func
 from uuid import uuid4
@@ -31,8 +31,13 @@ class Hotwheels(Base):
     notes = Column(String(1000))
     treasure_hunt_year = Column(Integer)
     super_treasure_hunt_year = Column(Integer)
+    visit_count = Column(Integer, default=0, nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.now())
     update_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
+    
+    wishlists = relationship("Wishlist", back_populates="hotwheels")
+    user_hotwheels = relationship("UserHotwheels", back_populates="hotwheels")
+    collections_items = relationship("CollectionItem", back_populates="hotwheels")
     
     __table_args__ = (
         Index('idx_hotwheels_model_name', model_name),
