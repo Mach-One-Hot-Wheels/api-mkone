fe5a7e11e19dd5a24ac023860f4c9f62156346cc
Breno <breno.cpedroso@gmail.com>
2025-03-15 20:01:22 -0300

    Add Wishlist model and API router for managing user wishlists

---
+++ src/wishlist/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, UUID, TIMESTAMP, ForeignKey
+from sqlalchemy.sql import func
+from uuid import uuid4
+from src.base import Base
+
+
+class Wishlist(Base):
+    __tablename__ = "wishlist"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    hotwheels_id = Column(UUID(as_uuid=True), ForeignKey('hotwheels.id'), nullable=False)
+
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    update_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
+
+    user = relationship("User", back_populates="wishlists")
+    hotwheels = relationship("Hotwheels", back_populates="wishlists")
