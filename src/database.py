3f7cb5ea6f35beaef2469586eee6eb1f2812c94e
Breno <breno.cpedroso@gmail.com>
2025-03-15 20:01:04 -0300

    Add function to import Hotwheels data from CSV into the database

--- src/database.py
+++ src/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy.orm import Session
 from src.auth.models import User
 from src.hotwheels.models import Hotwheels
+from src.collections.models import Collection, CollectionItem
+from src.wishlist.models import Wishlist
+from src.user_hotwheels.models import UserHotwheels, UserHotwheelsSale
 from src.base import Base
 from dotenv import load_dotenv
+import pandas as pd
 import os
 
 load_dotenv()
@@ -14,7 +18,7 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOSTNAME = os.getenv("POSTGRES_HOSTNAME")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
-#DATABASE_URL = "sqlite:///database.sqlite"
+
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOSTNAME}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
@@ -29,5 +33,12 @@ def get_session():
         yield session
 
 
+def add_hotwheels():
+    df = pd.read_csv("other/hotwheels.csv")
+    df.to_sql("hotwheels", engine, if_exists="append", index=False)
+
+
 if __name__ == "__main__":
     create_database()
+    add_hotwheels()
+    print("Database created successfully")
